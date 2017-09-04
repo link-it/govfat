1f706f013df9d134dd43ac20bf88f91600f551be
Giovanni Bussu <bussu@link.it>
2017-09-04 17:03:51 +0200

    inserimento lotti fatture attive

--- web/commons/src/main/java/org/govmix/proxy/fatturapa/web/commons/consegnaFattura/InserimentoLotti.java
+++ web/commons/src/main/java/org/govmix/proxy/fatturapa/web/commons/consegnaFattura/InserimentoLotti.java
@@ -2,8 +2,16 @@ package org.govmix.proxy.fatturapa.web.commons.consegnaFattura;
 
 import java.util.List;
 
+import org.govmix.proxy.fatturapa.web.commons.businessdelegate.FatturaAttivaBD;
+
 public class InserimentoLotti {
 
+	private FatturaAttivaBD fatturaAttivaBD;
+	
+	public InserimentoLotti(FatturaAttivaBD fatturaAttivaBD) {
+		this.fatturaAttivaBD = fatturaAttivaBD;
+	}
+	
 	public InserimentoLottoResponse inserisciLotto(List<InserimentoLottoRequest> request) {
 		//TODO
 		return null;
