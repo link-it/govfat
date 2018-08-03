36e827b7c332b8090ad95c48f90d90e47c8d4177
Giuliano Pintori <pintori@link.it>
2018-08-03 12:10:59 +0200

    Corretta lettura file di properties configurazione.

--- web/commons/src/main/java/org/govmix/fatturapa/parer/utils/ConservazioneProperties.java
+++ web/commons/src/main/java/org/govmix/fatturapa/parer/utils/ConservazioneProperties.java
@@ -1,10 +1,14 @@
 package org.govmix.fatturapa.parer.utils;
 
 import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Properties;
 
+import org.govmix.proxy.fatturapa.web.commons.utils.CommonsProperties;
+import org.govmix.proxy.pcc.fatture.utils.AbstractProperties;
 
-public class ConservazioneProperties {
+
+public class ConservazioneProperties extends AbstractProperties{
 
 	private String urlParER;
 	private String username;
@@ -17,45 +21,74 @@ public class ConservazioneProperties {
 	private String userIDVersatore;
 	
 	private static final String PATH = "/conservazione.properties";
+	
+	/* ********  F I E L D S  P R I V A T I  ******** */
+
+	/** Reader delle proprieta' impostate nel file 'server.properties' */
+	private Properties reader;
 
 	private static ConservazioneProperties instance;
 	
 	public static ConservazioneProperties getInstance() throws Exception {
 		if(instance == null) {
-			instance = new ConservazioneProperties();
+			instance = new ConservazioneProperties(PATH);
 		}
 		return instance;
 	}
 	
-	private ConservazioneProperties() throws Exception {
-		Properties props = new Properties();
-		InputStream is = ConservazioneProperties.class.getResourceAsStream(PATH);
-		props.load(is);
+	private ConservazioneProperties(String path) throws Exception {
+		super(path);
 		
-		this.versioneServizio = getProperty(props, "org.govmix.proxy.fatturapa.conservazione.versioneServizio");
-		this.ambienteVersatore = getProperty(props, "org.govmix.proxy.fatturapa.conservazione.ambienteVersatore");
+		Properties propertiesReader = new Properties();
+		java.io.InputStream properties = null;
+		try{  
+			properties = CommonsProperties.class.getResourceAsStream(path);
+			if(properties==null){
+				throw new Exception("Properties "+path+" not found");
+			}
+			propertiesReader.load(properties);
+			properties.close();
+		}catch(java.io.IOException e) {
+			log.error("Riscontrato errore durante la lettura del file '"+path+"': "+e.getMessage(),e);
+			try{
+				if(properties!=null)
+					properties.close();
+			}catch(Exception er){}
+			throw e;
+		}	
 
-		this.userIDVersatore = getProperty(props, "org.govmix.proxy.fatturapa.conservazione.userIDVersatore");
-		this.urlParER = getProperty(props, "org.govmix.proxy.fatturapa.conservazione.urlParER");
+		this.reader = propertiesReader;
 		
-		this.username = getProperty(props, "org.govmix.proxy.fatturapa.conservazione.username");
-		this.password = getProperty(props, "org.govmix.proxy.fatturapa.conservazione.password");
+		this.versioneServizio = getProperty("org.govmix.proxy.fatturapa.conservazione.versioneServizio",true);
+		this.ambienteVersatore = getProperty("org.govmix.proxy.fatturapa.conservazione.ambienteVersatore",true);
+
+		this.userIDVersatore = getProperty( "org.govmix.proxy.fatturapa.conservazione.userIDVersatore",true);
+		this.urlParER = getProperty( "org.govmix.proxy.fatturapa.conservazione.urlParER",true);
+		
+		this.username = getProperty( "org.govmix.proxy.fatturapa.conservazione.username",true);
+		this.password = getProperty( "org.govmix.proxy.fatturapa.conservazione.password",true);
 		
 	}
 	
+//	
+//	private String getProperty(Properties props, String name) throws Exception {
+//		return getProperty(props, name, true);
+//	}
+//	
+//	private String getProperty(Properties props, String name, boolean mandatory) throws Exception {
+//		if(props.containsKey(name)){
+//			return props.getProperty(name).trim();
+//		} else {
+//			if(mandatory)
+//				throw new Exception("Property ["+name+"] non trovata");
+//		}
+//		return null;
+//	}
 	
-	private String getProperty(Properties props, String name) throws Exception {
-		return getProperty(props, name, true);
-	}
-	
-	private String getProperty(Properties props, String name, boolean mandatory) throws Exception {
-		if(props.containsKey(name)){
-			return props.getProperty(name).trim();
-		} else {
-			if(mandatory)
-				throw new Exception("Property ["+name+"] non trovata");
-		}
-		return null;
+	/* ********  P R O P E R T I E S  ******** */
+
+	public Enumeration<?> keys(){
+		return this.reader.propertyNames();
 	}
 	
 	public String getVersioneServizio() {
