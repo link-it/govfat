4d5856c25ba3b3663cb54ca5f370f9706c4c2793
Giovanni Bussu <bussu@link.it>
2018-10-11 16:45:12 +0200

    dipartimento registri

--- web/commons/src/main/java/org/govmix/fatturapa/parer/utils/CacheEnti.java
+++ web/commons/src/main/java/org/govmix/fatturapa/parer/utils/CacheEnti.java
@@ -1,5 +1,8 @@
 package org.govmix.fatturapa.parer.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.govmix.proxy.fatturapa.orm.Dipartimento;
 import org.govmix.proxy.fatturapa.orm.Ente;
@@ -11,24 +14,28 @@ public class CacheEnti {
 
 	private DipartimentoBD dipartimentoBD;
 	private EnteBD enteBD;
-	private Logger log;
+//	private Logger log;
+	
+	Map<String, Ente> mapEnti;
+	
 	public CacheEnti(Logger log) throws Exception {
 		this.enteBD = new EnteBD(log);
 		this.dipartimentoBD = new DipartimentoBD(log);
-		this.log = log;
+//		this.log = log;
+		this.mapEnti = new HashMap<String, Ente>();
 	}
 	
 	public Ente getEnte(String codiceDipartimento) throws Exception {
 		
-		IdDipartimento idDipartimento = new IdDipartimento();
-		idDipartimento.setCodice(codiceDipartimento);
-		this.log.debug("Cerco ente per codice dipartimento ["+codiceDipartimento+"]");
-		Dipartimento dipartimento = this.dipartimentoBD.get(idDipartimento);
-		this.log.debug("Cerco ente per codice dipartimento ["+codiceDipartimento+"], ente ["+dipartimento.getEnte().getNome()+"]");
-		Ente ente = this.enteBD.get(dipartimento.getEnte());
-		this.log.debug("Trovato ente ["+ente.getNome()+"] per codice dipartimento ["+codiceDipartimento+"]");
-
-		return ente;
+		if(!this.mapEnti.containsKey(codiceDipartimento)) {
+			IdDipartimento idDipartimento = new IdDipartimento();
+			idDipartimento.setCodice(codiceDipartimento);
+			Dipartimento dipartimento = this.dipartimentoBD.get(idDipartimento);
+			Ente ente = this.enteBD.get(dipartimento.getEnte());
+			this.mapEnti.put(codiceDipartimento, ente);
+		}
+		
+		return this.mapEnti.get(codiceDipartimento);
 	}
 
 }
