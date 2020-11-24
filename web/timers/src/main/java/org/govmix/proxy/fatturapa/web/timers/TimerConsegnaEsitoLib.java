634c6f13124f94f4f297024e607dbc1cd5b6dcca
Giovanni Bussu <bussu@link.it>
2020-11-24 18:19:40 +0100

    info debug

--- web/timers/src/main/java/org/govmix/proxy/fatturapa/web/timers/TimerConsegnaEsitoLib.java
+++ web/timers/src/main/java/org/govmix/proxy/fatturapa/web/timers/TimerConsegnaEsitoLib.java
@@ -85,24 +85,23 @@ public class TimerConsegnaEsitoLib extends AbstractTimerLib {
 				BatchProperties properties = BatchProperties.getInstance();
 				CommonsProperties commonsProperties = CommonsProperties.getInstance(this.log);
 
+				InvioNotifica invioNotificaSDICoop = null;
+				InvioNotifica invioNotificaSPCoop = null;
+				
+				if(properties.getRicezioneEsitoURLSDICoop()!=null) {
+					invioNotificaSDICoop = new InvioNotifica(properties.getRicezioneEsitoURLSDICoop(), properties.getRicezioneEsitoUsernameSDICoop(), properties.getRicezioneEsitoPasswordSDICoop(), commonsProperties.getIdEgovHeaderSDICoop());
+				}
+				
+				if(properties.getRicezioneEsitoURLSPCoop()!=null) {
+					invioNotificaSPCoop = new InvioNotifica(properties.getRicezioneEsitoURLSPCoop(), properties.getRicezioneEsitoUsernameSPCoop(), properties.getRicezioneEsitoPasswordSPCoop(), commonsProperties.getIdEgovHeaderSPCoop());
+				}
+				InvioNotificaFactory factory = new InvioNotificaFactory(invioNotificaSPCoop, invioNotificaSDICoop, this.log);
 				try {
-					while(countNotificheElaborate < countNotifiche) {
+					while(lstNotifiche!=null && !lstNotifiche.isEmpty()) {
 						for(NotificaEsitoCommittente notifica : lstNotifiche) {
 
 							try{
-
-								InvioNotifica invioNotificaSDICoop = null;
-								InvioNotifica invioNotificaSPCoop = null;
 								
-								if(properties.getRicezioneEsitoURLSDICoop()!=null) {
-									invioNotificaSDICoop = new InvioNotifica(properties.getRicezioneEsitoURLSDICoop(), properties.getRicezioneEsitoUsernameSDICoop(), properties.getRicezioneEsitoPasswordSDICoop(), commonsProperties.getIdEgovHeaderSDICoop());
-								}
-								
-								if(properties.getRicezioneEsitoURLSPCoop()!=null) {
-									invioNotificaSPCoop = new InvioNotifica(properties.getRicezioneEsitoURLSPCoop(), properties.getRicezioneEsitoUsernameSPCoop(), properties.getRicezioneEsitoPasswordSPCoop(), commonsProperties.getIdEgovHeaderSPCoop());
-								}
-								InvioNotificaFactory factory = new InvioNotificaFactory(invioNotificaSPCoop, invioNotificaSDICoop, this.log);
-
 								NotificaECRequest request = new NotificaECRequest();
 								request.setNotifica(notifica);
 								NotificaECResponse invioNotificaResponse = factory.invia(request, TimerConsegnaEsitoLib.isSPCoop(notifica.getFatturaElettronica().getLottoFatture()));
