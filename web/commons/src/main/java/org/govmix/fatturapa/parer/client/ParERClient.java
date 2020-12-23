a46eb960fc56722b3ff7de4155de13ddd37bacec
Giovanni Bussu <bussu@link.it>
2020-12-23 12:09:00 +0100

    tls2

--- web/commons/src/main/java/org/govmix/fatturapa/parer/client/ParERClient.java
+++ web/commons/src/main/java/org/govmix/fatturapa/parer/client/ParERClient.java
@@ -130,7 +130,7 @@ public class ParERClient {
 		        // Allow TLSv1 protocol only
 		        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
 		                sslcontext,
-		                new String[] { "TLSv2" },
+		                new String[] { "TLSv1.2" },
 		                null,
 		                SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
 		        client = HttpClients.custom().setSSLSocketFactory(sslsf).setDefaultRequestConfig(RequestConfig.custom()
